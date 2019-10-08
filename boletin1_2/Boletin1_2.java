0882288ac7d635be40fab5c9fc47c37c0a2b6a49
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-08 08:52:09 +0200

    Segundo ejercicio del boletin 1

---
+++ boletin1_2/Boletin1_2.java
@@ -0,0 +1,15 @@
+
+package boletin1_2;
+
+
+public class Boletin1_2 {
+
+  
+    public static void main(String[] args) {
+      int lado = 3, area;
+      area = lado*lado;
+      System.out.println("area = "+area);
+        
+    }
+    
+}
